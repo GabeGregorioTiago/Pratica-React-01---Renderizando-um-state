433dec81a6807a16a32ee44cd7f6acfd38595df8
GabeGregorioTiago <gabrielgregoriopereiratiago@gmail.com>
2022-07-09 16:08:32 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+// Crie uma class component 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Guarde em um state as seguintes propriedades: nome, idade, comida favorita, e uma array com pelo menos 3 músicas.
+
+// Renderize na tela:
+// Seu nome em um h1
+// Sua idade em um h2
+// Sua comida favorita em um h3
+// Suas músicas favoritas em uma lista (ul / ol)
+
+import React, { Component} from 'react';
+import logo from './fruta_favorita.jpeg';
+//import './App.css';
+//import ProgressiveImage from "react-progressive-image";
+
+
+
+//<img src={logo} className="App-logo" alt="logo" />
+class dados extends Component {
+
+    state = {
+       sobre:{
+           nome:'Gabriel',
+           idade: 24,
+           comida: 'Baião de dois',
+           musica:["Sugar we're going down","I write sins, not tragedies","New perspective"] 
+         }
+
+       };
+      
+       
+    
+      render() {
+        return (
+           <div>
+              <h1>Meu nome é {this.state.sobre.nome}</h1>
+              <h2>Tenho {this.state.sobre.idade} anos</h2>
+               <h3>Minha comida favorita é {this.state.sobre.comida}</h3>
+              <nav>
+                <ul>
+                  <li>{this.state.sobre.musica[0]}</li>
+                  <li>{this.state.sobre.musica[1]}</li>
+                  <li>{this.state.sobre.musica[2]}</li>
+                </ul>
+              </nav>
+              <img src={logo} alt="Banana, a minha fruta favorita"></img>
+           </div>
+
+
+        )
+         
+    }
+
 }
 
-export default App;
+export default dados;
+
+
+//**Bônus
+// Adicione uma imagem da sua fruta favorita via import
+
